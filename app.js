f70f86ae81b6f023cf25b4ac01b602c297b09b68
stealthman22 <tdotonline@gmail.com>
2020-07-03 23:40:23 +0000

    messing around a bit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // select root nodes
+// This is my first project after almost staying of js coding
+// for about two minths
+// was actually working on my ds diploma
+// funny cos i actually forgot how to comment in js
+// kept using #
+
+let a = alert('I have almost forgeotten how sweet js is, python is bae, js is mama');
+console.log(a);
