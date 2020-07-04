c05b0f9a8e22f524995dab93b3146b755bd74b43
stealthman22 <tdotonline@gmail.com>
2020-07-04 14:29:04 +0000

    building generator functions

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 // select root nodes
 // This is my first project after almost staying of js coding
-// for about two minths
+// for about two months
 // was actually working on my ds diploma
 // funny cos i actually forgot how to comment in js
 // kept using #
 
-let a = alert('I have almost forgeotten how sweet js is, python is bae, js is mama');
-console.log(a);
+// Generator funtions
+
+// generate lowercase
+function getRandomLower() {
+    // cos we want 26 letters
+    // + 97 so we generate only codes in lower case char range
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+}
+
+// generate uppercase
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+}
+
+// generatenumbers
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+}
+
+// generate symbols
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+}
+
+console.log(getRandomNumber());
